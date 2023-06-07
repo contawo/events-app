f55e574072059b43db074e5e0682a97a61363f3b
Awonke11 <93478189+Awonke11@users.noreply.github.com>
2023-06-07 15:25:28 +0200

    UI Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="manifest.json" />
         <meta name="theme-color" content="#000000"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
